8bff56111b74e5c53f7170ca85609d82e2f1b053
Sojung Choi <sojc312gmail.com>
2020-10-14 11:57:57 -0700

    added surveyjs library

--- app-public/src/app/app.module.ts
+++ app-public/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SurveyComponent } from './survey.component';
+import { SurveyCreatorComponent } from './survey.creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyComponent,
+    SurveyCreatorComponent
   ],
   imports: [
     BrowserModule,
