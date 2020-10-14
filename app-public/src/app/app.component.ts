9ec30e6d002ab5e53047554edbf147a27a5ebac9
Sojung Choi <sojc312gmail.com>
2020-10-14 13:40:18 -0700

    added home pag

--- app-public/src/app/app.component.ts
+++ app-public/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Survey } from 'survey-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-public';
+  title = 'Sample Survey';
   json = {
 
+
   };
-}
+
+;}
--- app-public/src/app/app.module.ts
+++ app-public/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SurveyComponent } from './survey.component';
 import { SurveyCreatorComponent } from './survey.creator.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SurveyComponent,
-    SurveyCreatorComponent
+    SurveyCreatorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule, 
+    MatIconModule, 
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
